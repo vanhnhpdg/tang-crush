78659d0f50ca9e6c6d053331fb67948e695f73ac
vanhnhpdg <86811054+vanhnhpdg@users.noreply.github.com>
2021-07-02 10:49:49 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,17 +1,17 @@
 const CONFIG = {
     introTitle: 'Thảo B à!',
-    introDesc: `Trái đất vốn lạ thường
+    introDesc: 'Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    Đằng sau chữ yêu đây là thương',
     btnIntro: 'hihi',
     title: 'Chào bạn cùng pàn khoảng thời gian ngồi với tớ cậu có zui hông 😙',
-    desc: 'tớ rất zui vì ngồi với cậu :))) ',
+    desc: 'Tớ rất zui vì ngồi với cậu :)))',
     btnYes: 'Có zui chứ <33',
     btnNo: 'Không :3',
     question:'Sao cậu lại zui khi ngồi với tớ z :))) <3',
     btnReply: 'Gửi cho Vanh cute <3',
     reply: 'Thích thì ngồi không thích cũng ngồi <33333333',
-    mess: 'Tớ biết mà 🥰. HIHI 😘😘',
+    mess: 'Tớ biết mà 🥰. Hihi 😘😘',
     messLink : 'htt://m.me/emm.van.583
 }
